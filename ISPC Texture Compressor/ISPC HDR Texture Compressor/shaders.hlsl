d425cc71cc006906a306faa1972ec2504127b4bd
codergirl42 <marissa@galacticsoft.net>
2017-06-05 15:41:06 -0700

    Updated License Headers
    Fixed control characters that caused misformatting in some text editors

--- ISPC Texture Compressor/ISPC HDR Texture Compressor/shaders.hlsl
+++ ISPC Texture Compressor/ISPC HDR Texture Compressor/shaders.hlsl
@@ -1,19 +1,18 @@
-//--------------------------------------------------------------------------------------
-// Copyright 2013 Intel Corporation
-// All Rights Reserved
+////////////////////////////////////////////////////////////////////////////////
+// Copyright 2017 Intel Corporation
 //
-// Permission is granted to use, copy, distribute and prepare derivative works of this
-// software for any purpose and without fee, provided, that the above copyright notice
-// and this statement appear in all copies.  Intel makes no representations about the
-// suitability of this software for any purpose.  THIS SOFTWARE IS PROVIDED "AS IS."
-// INTEL SPECIFICALLY DISCLAIMS ALL WARRANTIES, EXPRESS OR IMPLIED, AND ALL LIABILITY,
-// INCLUDING CONSEQUENTIAL AND OTHER INDIRECT DAMAGES, FOR THE USE OF THIS SOFTWARE,
-// INCLUDING LIABILITY FOR INFRINGEMENT OF ANY PROPRIETARY RIGHTS, AND INCLUDING THE
-// WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.  Intel does not
-// assume any responsibility for any errors which may appear in this software nor any
-// responsibility to update it.
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not
+// use this file except in compliance with the License.  You may obtain a copy
+// of the License at
 //
-//--------------------------------------------------------------------------------------
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+// WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+// License for the specific language governing permissions and limitations
+// under the License.
+////////////////////////////////////////////////////////////////////////////////
 
 Texture2D gTexture : register(t0);
 Texture2D gCompressedTexture : register(t1);
